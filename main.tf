a834359b9666b355f31be71d0cf030f72c8c45f9
ARIES5533 <olumokomoses94@gmail.com>
2024-09-27 00:10:07 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "7cf7850e-72ff-47e7-818a-efe7f01e30ef"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "North Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_A2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
